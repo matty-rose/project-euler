1c937ef0b7e2c7b4f6fd8deffd66cf4cb8d7d0d9
Matthew Rose <matthew.rose@maxkelsen.com>
2021-03-28 15:45:27 +1000

    p1

---
+++ multiples3and5/Main.hs
@@ -0,0 +1,8 @@
+isMultiple :: Integer -> Bool
+isMultiple a
+  | a `mod` 3 == 0 = True
+  | a `mod` 5 == 0 = True
+  | otherwise = False
+
+main :: IO ()
+main = print $ sum $ filter isMultiple [1 .. 999]
