eaba18ab02d570a79b200d938004c0ae795d3e4f
Matthew Rose <matthew.rose@maxkelsen.com>
2021-03-31 19:23:09 +1000

    prime-sum speedup

--- prime-sum/Main.hs
+++ prime-sum/Main.hs
@@ -4,4 +4,4 @@ isPrime a = all (\x -> a `mod` x /= 0) [2 .. roota]
     roota = floor $ sqrt $ fromIntegral a
 
 main :: IO ()
-main = print $ sum $ filter isPrime [2 .. 2000000]
+main = print $ sum $ filter isPrime $ 2 : (tail [1,3 .. 2000000])
